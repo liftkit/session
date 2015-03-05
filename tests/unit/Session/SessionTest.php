a5c19de850b4c4510989307b5949186369aaa9a6
Ryan Williams <rwilliams@stream9.net>
2015-03-05 15:37:48 -0500

    Improved tests

--- tests/unit/Session/SessionTest.php
+++ tests/unit/Session/SessionTest.php
@@ -2,19 +2,19 @@
 
 
 	namespace LiftKit\Tests\Unit\Session;
-	
+
 	use LiftKit\Tests\Helpers\Session\TestCase;
 	use LiftKit\Tests\Stub\Session\Session;
-	
-	
+
+
 	class SessionTest extends TestCase
 	{
 		/**
 		 * @var Session
 		 */
 		protected $session;
-		
-		
+
+
 		public function setUp ()
 		{
 			$this->session = new Session(array(
@@ -22,8 +22,8 @@
 				'value2' => 2,
 			));
 		}
-		
-		
+
+
 		public function testSetGetUnsetSession ()
 		{
 			$this->session['value3'] = 3;
@@ -32,9 +32,11 @@
 				3,
 				$this->session['value3']
 			);
-			
+
+			$this->assertTrue(isset($this->session['value3']));
+
 			unset($this->session['value3']);
-			
+
 			$this->assertFalse(isset($this->session['value3']));
 		}
 	}
