c17b98ca13a85e549be3ec6615be1b553cca7ccd
Ryan Williams <rwilliams@stream9.net>
2015-03-12 19:03:14 -0400

    Fixed initial values in stub

--- tests/stub/Session/Session.php
+++ tests/stub/Session/Session.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
 
 	namespace LiftKit\Tests\Stub\Session;
-	
+
 	use LiftKit\Session\Session as BaseSession;
-	
-	
+
+
 	class Session extends BaseSession
 	{
-		
-		
-		public function __construct ($initalValue = array())
+
+
+		public function __construct ($initialValue = array())
 		{
 			$_SESSION = $initialValue;
 		}
-		
-		
+
+
 		public function start ()
 		{
 			$_SESSION = array();
 		}
-		
-		
+
+
 		public function destroy ()
 		{
 			unset($_SESSION);
